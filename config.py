7498de3ff015bee8e9e76b28677477f727c4862c
dmitriy.yartsev <dmitriy.yartsev@axenix.pro>
2024-06-15 02:42:29 +0300

    replace auth_token by .env

--- config.py
+++ config.py
@@ -1,9 +1,13 @@
 from GigaChat import GigaChat
 from PIL import Image 
+import os
+import dotenv
+
+dotenv.load_dotenv()
 
 params = {'img_path': '.../logo.png',\
           'system_prompt' : 'Отвечай как будто ты работник госуслуг',\
-          'auth_token': 'ZGUxYmViNDQtYTgyZS00OGM3LWI0Y2YtYWJmMjY3ZjZlYTY2OjM0NGVlYzVmLWIwYmItNGI1YS1hZmZiLTFiNzNhZmY1ZmEzNg=='}
+          'auth_token': os.environ.get('AUTH_TOKEN')}
 
 def init():
 
